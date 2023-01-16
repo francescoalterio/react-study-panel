f8256ad634dbc2047ab729c2f126313f6da36c77
francescoalterio <francescoalteriog@gmail.com>
2023-01-16 15:16:25 -0400

    fix bug in filterTechnologyBySearch

--- src/utils/filterTechnologiesBySearch.js
+++ src/utils/filterTechnologiesBySearch.js
@@ -1,5 +1,5 @@
 export function filterTechnologiesBySearch(technologies, search) {
-  technologies.filter((tech) => {
+  return technologies.filter((tech) => {
     const lowerCaseTech = tech.technology.toLowerCase();
     const lowerCaseSearchInput = search.toLowerCase();
     return lowerCaseTech.includes(lowerCaseSearchInput);
